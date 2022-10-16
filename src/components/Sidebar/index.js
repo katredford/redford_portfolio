c485f5abdfcb9a2639c9be84a940b6fdabc0c602
katredford <katharine.redford@gmail.com>
2022-10-15 19:14:15 -0500

    add sidebar component

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,15 @@
+import { Link } from 'react-router-dom'
+import './index.scss'
+
+
+const Sidebar = () => (
+  
+  <div className="nav-bar">
+    <Link className='logo' to='/'>
+      <img src={ } />
+    </Link>
+  </div>
+
+)
+
+export default Sidebar
